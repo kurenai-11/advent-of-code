e4c103a93a35a15a062117bbb67087f9c7aeb2d8
kurenai-11 <moldrenkailich@gmail.com>
2023-12-02 17:36:21 +0300

    deleted unneeded stuff from part2

--- 2023/day_1/src/bin/part2.rs
+++ 2023/day_1/src/bin/part2.rs
@@ -1,35 +1,11 @@
-pub const EXAMPLE1_INPUT: &str = include_str!("example.txt");
 pub const REAL_INPUT: &str = include_str!("input.txt");
 pub const EXAMPLE2_INPUT: &str = include_str!("example_2.txt");
 
 fn main() {
-    let example1_result = part1(EXAMPLE1_INPUT);
     let example2_result = part2(EXAMPLE2_INPUT);
-    let result1 = part1(REAL_INPUT);
     let result2 = part2(REAL_INPUT);
-    println!("example1 result: {}", example1_result);
     println!("example2 result: {}", example2_result);
-    println!("result1: {result1}");
     println!("result2: {result2}");
-}
-
-pub fn part1(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|l| l.chars().filter_map(|v| v.to_digit(10)))
-        .map(|mut r| {
-            let first = r.next();
-            let last = r.last();
-            if let (Some(val1), Some(val2)) = (first, last) {
-                format!("{}{}", val1, val2)
-            } else if let (Some(val1), None) = (first, last) {
-                format!("{}{}", val1, val1)
-            } else {
-                panic!("the string has no numbers at all")
-            }
-        })
-        .map(|p| p.parse::<u32>().unwrap())
-        .sum()
 }
 
 #[derive(Debug)]
@@ -82,18 +58,6 @@ pub fn part2(input: &str) -> u32 {
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn p1_example_works() {
-        let result = part1(EXAMPLE1_INPUT);
-        assert_eq!(result, 142)
-    }
-
-    #[test]
-    fn p1_input_works() {
-        let result = part1(REAL_INPUT);
-        assert_eq!(result, 53921);
-    }
 
     #[test]
     fn p2_example_works() {
